4acfb089674808effebae38f0ecb6f69548defdb
Matt Dudzik <mdudzik92@gmail.com>
2021-08-07 04:16:17 -0500

    Updating CSS through JS DOM manipulation when winning the game

--- script.js
+++ script.js
@@ -19,17 +19,38 @@ document.querySelector(".check").addEventListener("click", () => {
 	const guess = Number(document.querySelector(".guess").value);
 	console.log(guess, typeof guess);
 
+	// When there is no input
 	if (!guess) {
 		document.querySelector(".message").textContent = "⛔ No number!";
+
+		// When player wins
 	} else if (guess === secretNumber) {
 		document.querySelector(".message").textContent = "🎉 Correct Number!";
+
+		document.querySelector("body").style.backgroundColor = "#60b347";
+
+		document.querySelector(".number").style.width = "30rem";
+
+		// When guess is too high
 	} else if (guess > secretNumber) {
-		document.querySelector(".message").textContent = "📈 Too high!";
-		score--;
-		document.querySelector(".score").textContent = score;
+		if (score > 1) {
+			document.querySelector(".message").textContent = "📈 Too high!";
+			score--;
+			document.querySelector(".score").textContent = score;
+		} else {
+			document.querySelector(".message").textContent = "💥 You lost the game!";
+			document.querySelector(".score").textContent = 0;
+		}
+
+		// When guess is too low
 	} else if (guess < secretNumber) {
-		document.querySelector(".message").textContent = "📉 Too Low!";
-		score--;
-		document.querySelector(".score").textContent = score;
+		if (score > 1) {
+			document.querySelector(".message").textContent = "📉 Too low!";
+			score--;
+			document.querySelector(".score").textContent = score;
+		} else {
+			document.querySelector(".message").textContent = "💥 You lost the game!";
+			document.querySelector(".score").textContent = 0;
+		}
 	}
 });
