1b33f0ce755857c75e42b5c51f78e0ba11cf38a4
Matt Dudzik <mdudzik92@gmail.com>
2021-08-07 03:42:06 -0500

    Generating a random number on page reload

--- script.js
+++ script.js
@@ -8,6 +8,11 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
+// Math.trunc cuts off the decimals
+// Adding +1 to make sure it includes 20
+const number = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(".number").textContent = number;
+
 document.querySelector(".check").addEventListener("click", () => {
 	const guess = Number(document.querySelector(".guess").value);
 	console.log(guess, typeof guess);
