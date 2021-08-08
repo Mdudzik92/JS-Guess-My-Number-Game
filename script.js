20949dae79a3b9ee11531c87e43b18de0ad1c4ef
Matt Dudzik <mdudzik92@gmail.com>
2021-08-08 02:59:25 -0500

    Refactoring messages with displayMessage function

--- script.js
+++ script.js
@@ -14,16 +14,20 @@ let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function (message) {
+	document.querySelector(".message").textContent = message;
+};
+
 document.querySelector(".check").addEventListener("click", () => {
 	const guess = Number(document.querySelector(".guess").value);
 
 	// When there is no input
 	if (!guess) {
-		document.querySelector(".message").textContent = "⛔ No number!";
+		displayMessage("⛔ No number!");
 
 		// When player wins
 	} else if (guess === secretNumber) {
-		document.querySelector(".message").textContent = "🎉 Correct Number!";
+		displayMessage("🎉 Correct Number!");
 
 		document.querySelector(".number").textContent = secretNumber;
 
@@ -39,12 +43,11 @@ document.querySelector(".check").addEventListener("click", () => {
 		// When guess is wrong
 	} else if (guess !== secretNumber) {
 		if (score > 1) {
-			document.querySelector(".message").textContent =
-				guess > secretNumber ? "📈 Too high!" : "📉 Too low!";
+			displayMessage(guess > secretNumber ? "📈 Too high!" : "📉 Too low!");
 			score--;
 			document.querySelector(".score").textContent = score;
 		} else {
-			document.querySelector(".message").textContent = "💥 You lost the game!";
+			displayMessage("💥 You lost the game!");
 			document.querySelector(".score").textContent = 0;
 		}
 	}
@@ -55,7 +58,7 @@ document.querySelector(".again").addEventListener("click", (e) => {
 	score = 20;
 
 	// Resetting initial values of the message, score, number and guess
-	document.querySelector(".message").textContent = "Start guessing...";
+	displayMessage("Start guessing...");
 	document.querySelector(".score").textContent = score;
 	document.querySelector(".number").textContent = "?";
 	document.querySelector(".guess").value = "";
